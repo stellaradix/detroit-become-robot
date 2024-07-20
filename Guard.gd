d0355af7ed950def7150dcd7f67ee150410b8495
iDot0 <eteepet@gmail.com>
2024-07-20 13:28:06 -0700

    Guard Update
    Added cliff detection and started on player detection.

--- Guard.gd
+++ Guard.gd
@@ -10,16 +10,19 @@ func _ready():
 	
 	#if direction == 1:
 	#$animatedsprite.flip_h = true
-	pass # Replace with function body.
+	
+	#Checks floor to not walk off edges
+	$FloorChecker.position.x = $CollisionShape2D.get_shape().size.x * direction
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
-	#swaps direction if theres a wall
-	if is_on_wall():
+	#swaps direction if theres a wall or no floor
+	if is_on_wall() or not $FloorChecker.is_colliding():
 		direction = direction * -1
 		#$animatedsprite.flip_h = not $animatedsprite.flip_h
+		$FloorChecker.position.x = $CollisionShape2D.get_shape().size.x * direction
 	
 	velocity.y += 20
 	
@@ -28,3 +31,11 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	
+
+
+func _on_light_body_entered(body):
+	#not detecting player
+	#its detecting the ledge as a player?
+	if body == $".":
+		print("ouch")
+
