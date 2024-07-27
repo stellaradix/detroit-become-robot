2591bb33738bf261dc1f1f826c0637b0aea1247b
iDot0 <eteepet@gmail.com>
2024-07-27 15:44:59 -0700

    Dialogue + Player Tweaks
    Note: Level doors currently do not work.

--- level_door.gd
+++ level_door.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
 @export var level_scene: String
-
+var DialogueFinished = false
 @onready var player = get_tree().get_first_node_in_group("player")
+@export var dialogue_resource: DialogueResource
+@export var tutorial_level = "res://tutorial_level.dialogue"
 
+func _process(_delta):
+	if DialogueFinished == true:
+		print("dialogue finished")
+		get_tree().change_scene_to_file(level_scene)
+		DialogueFinished = false
+		
 func _on_body_entered(body):
-	#temp door replace with another level in a desired order
 	if body == player:
 		print("player detected")
-		get_tree().change_scene_to_file(level_scene)
+		#doesnt work put this back if u need the level door to work again: get_tree().change_scene_to_file(level_scene)
+		DialogueManager.show_dialogue_balloon(dialogue_resource.tutorial_level)
