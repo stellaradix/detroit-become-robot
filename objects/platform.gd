0a460e457adc7a883f091103fb3815c4b0294d3d
IzuruTaika <aydenroynon13@gmail.com>
2024-07-29 20:39:34 -0500

    Wooden Platform

--- objects/platform.gd
+++ objects/platform.gd
@@ -1,13 +1,13 @@
 extends AnimatableBody2D
 
-@export var can_fall: bool
+@export var is_wooden: bool
 @onready var collision_shape_2d = $CollisionShape2D
 @onready var sprite_2d = $Sprite2D
 @onready var player = get_tree().get_first_node_in_group("player")
 
 func _process(_delta):
 	if MenuManager.is_changing == false:
-		if can_fall == true:
+		if is_wooden == true:
 			sprite_2d.texture = load("res://assets/wood_platform.png")
 			if player.is_crouching == true:
 				collision_shape_2d.disabled = true
